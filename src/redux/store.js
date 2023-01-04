2adf4d919778b81cf62a7815740668681ec2cb6a
AniaLaz <lazarenko.ania.sg@gmail.com>
2023-01-04 12:59:28 +0200

    middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
-import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
 import { contactsReducer } from '../redux/sliseContacts';
 import { filterReducer } from "../redux/sliseFilter"
+
+
 
 const persistConfig = {
   key: 'contacts',
@@ -19,6 +30,12 @@ export const store = configureStore({
     contacts: persistedReducer,
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
